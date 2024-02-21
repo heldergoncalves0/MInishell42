659bc893a526da1f5f55907eb138d336ef4a48be
Helder Fernando Bolonhes Goncalves <helferna@c2r2s1.42lisboa.com>
2024-02-21 16:04:08 +0000

    cd limpo

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,59 +6,34 @@
 /*   By: helferna <helferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 15:52:01 by helferna          #+#    #+#             */
-/*   Updated: 2024/02/21 15:28:29 by helferna         ###   ########.fr       */
+/*   Updated: 2024/02/21 15:53:28 by helferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void *go_one_up(t_shell *s)
+static void	handle_path(t_shell *s)
 {
-	size_t	old_size;
-	int		n;
-	char	*path;
-
-	path = NULL;
-	path = getcwd(path, 0);
-	set_env(s, "OLDPWD", get_env(s, "PWD"));
-	set_env(s, "PWD", path);
-	//return (path);
-}
-
-static void	*handle_path(t_shell *s, char *str)
-{
-	char	*pwd;
 	char	*tmp;
 
-	// if (ft_strncmp(str, "..", 3) == 0)
-	// 	go_one_up(s);
 	tmp = NULL;
 	set_env(s, "OLDPWD", get_env(s, "PWD"));
 	tmp = getcwd(tmp, 0);
 	set_env(s, "PWD", tmp);
+	free(tmp);
 }
 
 void	cd_cmd(t_cmd *cmd, t_shell *s, int in, int out)
 {
-	char	*pwd;
-	char	*tmp;
-	char    *path;
-
-	tmp = NULL;
-	path = NULL;
 	(void)in;
 	(void)out;
 
 	if (chdir(cmd->args[1]) == 0)
-		handle_path(s, cmd->args[1]);
+		handle_path(s);
 	else if (chdir(cmd->args[1]) != 0)
 	{
 		ft_putstr_fd("cd: not a directory: ", 2);
 		ft_putstr_fd(cmd->args[1], 2);
 		ft_putstr_fd("\n", 2);
 	}
-	// else if (ft_strncmp(cmd->args[1], ".", 3) == 0)
-	
-	free(path);
-	free(tmp);
 }
--- src/clean/exit_status.c
+++ src/clean/exit_status.c
@@ -6,7 +6,7 @@
 /*   By: helferna <helferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 14:11:03 by helferna          #+#    #+#             */
-/*   Updated: 2024/02/20 20:39:48 by helferna         ###   ########.fr       */
+/*   Updated: 2024/02/21 15:55:20 by helferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,3 @@ void	exit_status(t_shell *shell, int status)
 	shell->status = status;
 	exit(shell->status);
 }
-
-//echo "'$?'"
